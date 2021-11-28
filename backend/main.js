d99ff1aab8bed83330e730918d9209f7938a1ed6
dariohollbach <dario_hollbach@sluz.ch>
2021-11-28 10:47:41 +0100

    finished backend, delete still don't work

--- backend/main.js
+++ backend/main.js
@@ -35,10 +35,25 @@ router.get("/task", context => {
       context.response.status = 404
       context.response.body = `ID ${context.params.id} not found`
   } 
-}).delete("/task/:id", context => {
+}).put("/task/:id/down", context =>{
+  const index = kanbanTasks.findIndex(x => x.id == context.params.id)
+  if (index >= 0 && index < kanbanTasks.length){
+    if(kanbanTasks[index].state > 1 && kanbanTasks[index].state <= 3){
+      kanbanTasks[index].state--;
+      context.response.status = 202;
+    }
+    else{
+      context.response.status = 400;
+      context.response.body = `status could not be updated`
+    }
+} else {
+    context.response.status = 404
+    context.response.body = `ID ${context.params.id} not found`
+} 
+})
+.delete("/task/:id", context => {
   try {
     kanbanTasks = kanbanTasks.filter(x => x.id != context.params.id)
-    context.response.body = kanbanTasks;
   } catch (error) {
     context.response.body = error.message;
   }
